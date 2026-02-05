11a15670eda97c4075714a908e839f5cdad8665e
Oluwabukunmi07 <preciousbukunmi07@gmail.com>
2026-02-05 21:57:32 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
